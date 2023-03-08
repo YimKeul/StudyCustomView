671dddf4265420b5e52ccc229df64ccd449a3ca8
YimKeul <leesjun29@gmail.com>
2023-03-09 02:10:32 +0900

    Initial Commit

---
+++ StudyCustomView/StudyCustomViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyCustomViewApp.swift
+//  StudyCustomView
+//
+//  Created by yimkeul on 2023/03/09.
+//
+
+import SwiftUI
+
+@main
+struct StudyCustomViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
